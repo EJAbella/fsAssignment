235866bde39c913ac66f839eb991e88db05912c2
Elijah <elijah.abella@aol.com>
2018-07-31 11:15:18 -0700

    Completed all req's

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
 var fs = require('fs');
+
+// Challenge 1
+// let data = fs.readFileSync('./challenge1/info.txt', 'utf-8')
+// console.log(data)
+
+// Challenge 2
+// let data = fs.readFileSync('./challenge2/info.txt', 'utf-8')
+// data = data + 'Elijah\nSeth\nTodd\nAlbert\nLauren\nChelsea\nJake\nNhan\nDonnie\nJosh\nTyler'
+// fs.writeFileSync('./challenge2/info.txt', data)
+
+// Challenge 3
+// fs.renameSync('./challenge3/binfo.txt', './challenge3/info.txt')
+
+// Challenge 4
+// fs.mkdirSync('./challenge4/copyfolder')
+// let data = fs.readFileSync('./challenge4/info.txt', 'utf-8')
+// fs.writeFileSync('./challenge4/copyfolder/info.txt', data)
+
+// Challenge 5
+// let data = fs.readFileSync('./challenge5/info.txt', 'utf-8')
+// data = data.replace(/-/g, ' ')
+// fs.writeFileSync('./challenge5/info.txt', data)
+
+// Challenge 6
+// let files = fs.readdirSync('./challenge6')
+// files = files.filter((file) => file.match('.txt'))
+// files.forEach((file) => {
+//   console.log(fs.readFileSync(`./challenge6/${file}`, 'utf-8'))
+// })
